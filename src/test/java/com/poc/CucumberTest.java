c3d91a8e922a575baf8898a657f47dc012881d6b
Lokesh Raghuram <lokeshraghuram@gmail.com>
2019-12-25 23:37:36 +0530

    Update CucumberTest.java

--- src/test/java/com/poc/CucumberTest.java
+++ src/test/java/com/poc/CucumberTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true,
-        plugin = {"pretty", "html:target/cucumber"},
+        plugin = {"pretty", "json:target/cucumber.json"},
         features = "src/test/resources/features",
         extraGlue = "com.poc.bdd.component")
 public class CucumberTest {

7be4bc051e085033fd14e35fc379a794848da81b
Lokesh <lokesh.muppana@ikea.com>
2019-12-26 00:26:14 +0530

    changed options

--- src/test/java/com/poc/CucumberTest.java
+++ src/test/java/com/poc/CucumberTest.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true,
-        plugin = {"pretty", "json:target/cucumber.json"},
+@CucumberOptions(monochrome = false,
+        plugin = {"pretty","json:target/cucumber-report.json"},
         features = "src/test/resources/features",
         extraGlue = "com.poc.bdd.component")
 public class CucumberTest {
